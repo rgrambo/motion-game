6253771493eb1666b85495bb981998ea7a1eb852
Ross Grambo <rgrambo@uw.edu>
2016-02-19 02:53:27 -0800

    Working Commit

--- motiongame/app/src/main/java/edu/uw/rgrambo/motiongame/DrawingSurfaceView.java
+++ motiongame/app/src/main/java/edu/uw/rgrambo/motiongame/DrawingSurfaceView.java
@@ -5,14 +5,20 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * An example SurfaceView for generating graphics on
  * @author Joel Ross
+ * Editted by Ross Grambo
  * @version Winter 2016
  */
 public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
@@ -27,7 +33,36 @@ public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Cal
     private DrawingRunnable mRunnable; //the code htat we'll want to run on a background thread
     private Thread mThread; //the background thread
 
-    private Paint redPaint; //drawing variables (pre-defined for speed)
+    public List<Float> UpHits;
+    public List<Float> LeftHits;
+    public List<Float> DownHits;
+    public List<Float> RightHits;
+
+    private Paint blackPaint;
+    private Paint whitePaintLeft;
+    private Paint whitePaintRight;
+    private Paint redPaint;
+    private Paint bluePaint;
+    private Paint lightBluePaint;
+
+    private boolean LeftEnabled;
+    private boolean RightEnabled;
+    private boolean DownEnabled;
+
+    private int Rate;
+    private int Cooldown;
+    private int Level;
+    private int NextLevelRate;
+    private int NextLevelCooldown;
+
+    public int Health;
+    public int Points;
+
+    public boolean Playing;
+
+    public int ShakeCount;
+    private int ShakeCooldown;
+    private int ShakeCooldownRate;
 
 
     /**
@@ -52,9 +87,29 @@ public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Cal
 
         mRunnable = new DrawingRunnable();
 
-        //set up drawing variables ahead of timme
+        //set up drawing variables ahead of time
+        whitePaintLeft = new Paint(Paint.ANTI_ALIAS_FLAG);
+        whitePaintLeft.setColor(Color.WHITE);
+        whitePaintRight = new Paint(Paint.ANTI_ALIAS_FLAG);
+        whitePaintRight.setColor(Color.WHITE);
+
+        whitePaintLeft.setTextAlign(Paint.Align.LEFT);
+        whitePaintRight.setTextAlign(Paint.Align.RIGHT);
+
+        float textSize = whitePaintLeft.getTextSize();
+        whitePaintLeft.setTextSize(textSize * 2);
+        whitePaintRight.setTextSize(textSize * 2);
+
+        blackPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        blackPaint.setColor(Color.BLACK);
         redPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         redPaint.setColor(Color.RED);
+        bluePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        bluePaint.setColor(Color.BLUE);
+        lightBluePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        lightBluePaint.setColor(Color.argb(50, 0, 0, 255));
+
+        NewGame();
     }
 
 
@@ -62,10 +117,122 @@ public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Cal
      * Helper method for the "game loop"
      */
     public void update(){
-        //update the "game state" here (move things around, etc.
-        //TODO: fill in your own logic here!
+        if (Playing) {
+            Cooldown--;
+
+            if (ShakeCooldown > 0) {
+                ShakeCooldown --;
+            }
+
+            if (LeftEnabled && Cooldown == 150) {
+                SpawnLeft();
+            }
+
+            if (DownEnabled && Cooldown == 50) {
+                SpawnDown();
+            }
+
+            if (RightEnabled && Cooldown == 100) {
+                SpawnRight();
+            }
+
+            if (Cooldown == 25 || Cooldown == 75 || Cooldown == 125 || Cooldown == 175) {
+                SpawnUp();
+            }
+
+            // add to counter sound offset
+            if ((Cooldown + 16) % 25 == 0) {
+                ((MainActivity)getContext()).playTick();
+            }
+
+            if (Cooldown <= 0) {
+                Cooldown = Rate;
+                NextLevelCooldown--;
+                if (NextLevelCooldown <= 0) {
+                    Level++;
+                    NextLevelCooldown = NextLevelRate;
+                    if (Level == 2) {
+                        LeftEnabled = true;
+                    } else if (Level == 3) {
+                        RightEnabled = true;
+                    } else if (Level == 4) {
+                        DownEnabled = true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < UpHits.size(); i++) {
+                UpHits.set(i, UpHits.get(i) - 2f);
+                if (UpHits.get(i) <= 30) {
+                    UpHits.remove(i);
+                    LoseHealth();
+                }
+            }
+
+            for (int i = 0; i < DownHits.size(); i++) {
+                DownHits.set(i, DownHits.get(i) - 2f);
+                if (DownHits.get(i) <= 30) {
+                    DownHits.remove(i);
+                    LoseHealth();
+                }
+            }
+
+            for (int i = 0; i < LeftHits.size(); i++) {
+                LeftHits.set(i, LeftHits.get(i) - 2f);
+                if (LeftHits.get(i) <= 30) {
+                    LeftHits.remove(i);
+                    LoseHealth();
+                }
+            }
+
+            for (int i = 0; i < RightHits.size(); i++) {
+                RightHits.set(i, RightHits.get(i) - 2f);
+                if (RightHits.get(i) <= 30) {
+                    RightHits.remove(i);
+                    LoseHealth();
+                }
+            }
+        }
     }
 
+    private void LoseHealth() {
+        Health--;
+        if (Health <= 0) {
+            GameOver();
+        }
+    }
+
+    private void GameOver() {
+        Playing = false;
+        ClearAll();
+
+        // Get a handler that can be used to post to the main thread
+        Handler mainHandler = new Handler((getContext()).getMainLooper());
+
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                ((MainActivity)getContext()).MenuButtons();
+            } // This is your code
+        };
+        mainHandler.post(myRunnable);
+    }
+
+    private void SpawnUp() {
+        UpHits.add(UpHits.size(), 1000f);
+    }
+
+    private void SpawnLeft() {
+        LeftHits.add(LeftHits.size(), 1000f);
+    }
+
+    private void SpawnRight() {
+        RightHits.add(RightHits.size(), 1000f);
+    }
+
+    private void SpawnDown() {
+        DownHits.add(DownHits.size(), 1000f);
+    }
 
     /**
      * Helper method for the "render loop"
@@ -73,21 +240,45 @@ public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Cal
      */
     public void render(Canvas canvas){
         if(canvas == null) return; //if we didn't get a valid canvas for whatever reason
-
-        //TODO: replace the below example with your own rendering
-
         canvas.drawColor(Color.BLACK); //black out the background
-        canvas.drawCircle(viewWidth/2.0f, viewHeight/2.0f, 100.0f, redPaint); //we can draw directly onto the canvas
-        //see http://developer.android.com/reference/android/graphics/Canvas.html for a list of options
 
-        for(int x=50; x<viewWidth-50; x++) { //most of the width
-            for(int y=100; y<110; y++) { //10 pixels high
-                bmp.setPixel(x, y, Color.BLUE); //we can also set individual pixels in a Bitmap (like a BufferedImage)
+        canvas.drawText("Health: " + Health, 100, 100, whitePaintLeft);
+        canvas.drawText("Points: " + Points, viewWidth - 100, 100, whitePaintRight);
+        canvas.drawText("Shake Events: " + ShakeCount, viewWidth - 100, viewHeight - 100, whitePaintRight);
+
+        canvas.drawCircle(viewWidth / 2.0f, viewHeight / 2.0f, 110.0f, lightBluePaint);
+        canvas.drawCircle(viewWidth / 2.0f, viewHeight / 2.0f, 65.0f, blackPaint);
+
+        try {
+            for (float f : UpHits) {
+                canvas.drawCircle(viewWidth / 2.0f, viewHeight / 2.0f - f, 15.0f, redPaint);
             }
-        }
-        //Canvas bmc = new Canvas(bmp); //we can also make a canvas out of a Bitmap to draw on that (like fetching g2d from a BufferedImage)
 
-        canvas.drawBitmap(bmp,0,0,null); //and then draw the BitMap onto the canvas.
+            for (float f : DownHits) {
+                canvas.drawCircle(viewWidth / 2.0f, viewHeight / 2.0f + f, 15.0f, redPaint);
+            }
+
+            for (float f : LeftHits) {
+                canvas.drawCircle(viewWidth / 2.0f - f, viewHeight / 2.0f, 15.0f, redPaint);
+            }
+
+            for (float f : RightHits) {
+                canvas.drawCircle(viewWidth / 2.0f + f, viewHeight / 2.0f, 15.0f, redPaint);
+            }
+        } catch (ConcurrentModificationException e) {
+            // Ignore it
+        }
+
+        canvas.drawCircle(viewWidth/2.0f, viewHeight/2.0f, 30.0f, bluePaint);
+
+//        for(int x=50; x<viewWidth-50; x++) { //most of the width
+//            for(int y=100; y<110; y++) { //10 pixels high
+//                bmp.setPixel(x, y, Color.BLUE); //we can also set individual pixels in a Bitmap (like a BufferedImage)
+//            }
+//        }
+//        //Canvas bmc = new Canvas(bmp); //we can also make a canvas out of a Bitmap to draw on that (like fetching g2d from a BufferedImage)
+//
+//        canvas.drawBitmap(bmp, 0, 0, null); //and then draw the BitMap onto the canvas.
     }
 
 
@@ -162,5 +353,48 @@ public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Cal
                 }
             }
         }
+    }
+
+    public void ShakeEvent() {
+        if (ShakeCount > 0) {
+            if (ShakeCooldown <= 0) {
+                ShakeCooldown = ShakeCooldownRate;
+                ShakeCount--;
+                ClearAll();
+            }
+        }
+    }
+
+    public void ClearAll() {
+        UpHits = new LinkedList<Float>();
+        LeftHits = new LinkedList<Float>();
+        RightHits = new LinkedList<Float>();
+        DownHits = new LinkedList<Float>();
+    }
+
+    public void NewGame() {
+        // Set up hits
+        UpHits = new LinkedList<Float>();
+        DownHits = new LinkedList<Float>();
+        LeftHits = new LinkedList<Float>();
+        RightHits = new LinkedList<Float>();
+
+        Rate = 200;
+        Cooldown = 500;
+        NextLevelRate = 4;
+        NextLevelCooldown = 5;
+        Level = 1;
+
+        Health = 10;
+
+        ShakeCount = 3;
+        ShakeCooldownRate = 200;
+        ShakeCooldown = ShakeCooldownRate;
+
+        LeftEnabled = false;
+        RightEnabled = false;
+        DownEnabled = false;
+
+        Playing = false;
     }
 }
--- motiongame/app/src/main/java/edu/uw/rgrambo/motiongame/MainActivity.java
+++ motiongame/app/src/main/java/edu/uw/rgrambo/motiongame/MainActivity.java
@@ -11,7 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         SensorEventListener,
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements
     // Accelermeter handling loosely based on
     // http://examples.javacodegeeks.com/android/core/hardware/sensor/android-accelerometer-example/
     private Sensor mAccelerometer;
+    private Sensor mSensor;
 
     private float lastX;
     private float lastY;
@@ -32,12 +39,27 @@ public class MainActivity extends AppCompatActivity implements
     private float deltaY = 0;
     private float deltaZ = 0;
 
+    private float shakeThreshold = 4;
+
+    private float soundRate = 1;
+
+    private DrawingSurfaceView drawingSurfaceView;
+
     // Gestures based off of http://developer.android.com/training/gestures/detector.html
     private String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
 
     SoundPool soundPool;
-    int music;
+
+    float[] mGravity;
+    float[] mGeomagnetic;
+
+    int Tick;
+    int Tap;
+    int SwipeLeft;
+    int SwipeRight;
+    int SwipeDown;
+    int Bomb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +90,36 @@ public class MainActivity extends AppCompatActivity implements
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                soundPool.play(music, 1, 1, 0, 0, 1);
+
             }
         });
 
-        music = soundPool.load(getBaseContext(), R.raw.sandstorm, 1);
+        Tick = soundPool.load(getBaseContext(), R.raw.tick, 1);
+        Tap = soundPool.load(getBaseContext(), R.raw.drum, 1);
+        SwipeRight = soundPool.load(getBaseContext(), R.raw.clap, 1);
+        SwipeLeft = soundPool.load(getBaseContext(), R.raw.cowbell, 1);
+        SwipeDown = soundPool.load(getBaseContext(), R.raw.crash, 1);
+        Bomb = soundPool.load(getBaseContext(), R.raw.bomb, 1);
+
+        drawingSurfaceView = ((DrawingSurfaceView)findViewById(R.id.surfaceView));
+
+        MenuButtons();
+        ((Button)findViewById(R.id.start)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawingSurfaceView.NewGame();
+                drawingSurfaceView.Playing = true;
+                PlayingButtons();
+            }
+        });
+        ((Button)findViewById(R.id.stop)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawingSurfaceView.Playing = false;
+                drawingSurfaceView.ClearAll();
+                MenuButtons();
+            }
+        });
     }
 
     @Override
@@ -81,10 +128,38 @@ public class MainActivity extends AppCompatActivity implements
         return super.onTouchEvent(event);
     }
 
+    public void PlayingButtons() {
+        ((Button)findViewById(R.id.start)).setEnabled(false);
+        ((Button)findViewById(R.id.stop)).setEnabled(true);
+    }
+
+    public void MenuButtons() {
+        ((Button)findViewById(R.id.start)).setEnabled(true);
+        ((Button)findViewById(R.id.stop)).setEnabled(false);
+    }
+
     @Override
     public boolean onFling(MotionEvent event1, MotionEvent event2,
                            float velocityX, float velocityY) {
         Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        float dx = event1.getX() - event2.getX();
+        float dy = event1.getY() - event2.getY();
+        if (Math.abs(dx) > Math.abs(dy)) {
+            if (dx > 0) {
+                List<Float> LeftHits = drawingSurfaceView.LeftHits;
+                DeleteBeat(LeftHits, SwipeLeft);
+            } else {
+                List<Float> RightHits = drawingSurfaceView.RightHits;
+                DeleteBeat(RightHits, SwipeRight);
+            }
+        } else {
+            if (dy < 0) {
+                List<Float> DownHits = drawingSurfaceView.DownHits;
+                DeleteBeat(DownHits, SwipeDown);
+            } else {
+                // do nothing for up
+            }
+        }
         return true;
     }
 
@@ -100,9 +175,8 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onSingleTapUp(MotionEvent event) {
-        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
-        float x = event.getRawX();
-        float y = event.getRawY();
+        List<Float> UpHits = drawingSurfaceView.UpHits;
+        DeleteBeat(UpHits, Tap);
         return true;
     }
 
@@ -116,10 +190,15 @@ public class MainActivity extends AppCompatActivity implements
 
     }
 
+    public void playTick() {
+        soundPool.play(Tick, 1, 1, 0, 0, 1);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -130,36 +209,49 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mGravity = event.values;
 
-        // Sets the deltas of the new values
-        deltaX = Math.abs(lastX - event.values[0]);
-        deltaY = Math.abs(lastY - event.values[1]);
-        deltaZ = Math.abs(lastZ - event.values[2]);
+            // Sets the deltas of the new values
+            deltaX = Math.abs(lastX - event.values[0]);
+            deltaY = Math.abs(lastY - event.values[1]);
+            deltaZ = Math.abs(lastZ - event.values[2]);
 
-        // A change below 2 is white noise
-        if (deltaX < 2) {
-            deltaX = 0;
+            // Sets the last known values
+            lastX = event.values[0];
+            lastY = event.values[1];
+            lastZ = event.values[2];
+
+            if (deltaX > shakeThreshold || deltaY > shakeThreshold || deltaZ > shakeThreshold) {
+                drawingSurfaceView.ShakeEvent();
+                if (drawingSurfaceView.Playing && drawingSurfaceView.ShakeCount > 0) {
+                    soundPool.play(Bomb, 1, 1, 0, 0, soundRate);
+                }
+            }
         }
-        if (deltaY < 2) {
-            deltaY = 0;
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mGeomagnetic = event.values;
         }
-
-        // Sets the last known values
-        lastX = event.values[0];
-        lastY = event.values[1];
-        lastZ = event.values[2];
-
-
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                Log.e("test", orientation[0] + "");
+            }
+        }
     }
 
     @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
-    }
+    public boolean onSingleTapConfirmed(MotionEvent e) {return false;}
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
@@ -169,5 +261,16 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
         return false;
+    }
+
+    private void DeleteBeat(List<Float> Hits, int sound) {
+        for (int i = 0; i < Hits.size(); i++) {
+            Hits.set(i, Hits.get(i) - 2f);
+            if (Hits.get(i) <= 100 && Hits.get(i) > 65) {
+                Hits.remove(i);
+                drawingSurfaceView.Points++;
+                soundPool.play(sound, 1, 1, 0, 0, soundRate);
+            }
+        }
     }
 }
